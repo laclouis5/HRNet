d6e93b069c59defb38fce12f7d6df300496e3638
Louis Lac <lac.louis5@gmail.com>
2023-06-05 10:09:56 +0200

    Misc

--- hrnet.py
+++ hrnet.py
@@ -8,7 +8,7 @@ def conv1x1(in_channels: int, out_channels: int) -> nn.Conv2d:
     )
 
 
-def conv3x3(in_channels: int, out_channels, stride: int = 1) -> nn.Conv2d:
+def conv3x3(in_channels: int, out_channels: int, stride: int = 1) -> nn.Conv2d:
     return nn.Conv2d(
         in_channels=in_channels,
         out_channels=out_channels,
@@ -95,19 +95,19 @@ class ExpandedBottleneck(nn.Module):
 
 
 class Up(nn.Sequential):
-    def __init__(self, r: int, in_channels: int, out_channels: int) -> None:
+    def __init__(self, d: int, in_channels: int, out_channels: int) -> None:
         super().__init__(
-            nn.UpsamplingBilinear2d(scale_factor=2**r),
+            nn.UpsamplingBilinear2d(scale_factor=2**d),
             conv1x1(in_channels=in_channels, out_channels=out_channels),
             nn.BatchNorm2d(num_features=out_channels),
         )
 
 
 class Down(nn.Sequential):
-    def __init__(self, r: int, in_channels: int, out_channels: int) -> None:
+    def __init__(self, d: int, in_channels: int, out_channels: int) -> None:
         layers = [
             Conv3x3Block(in_channels=in_channels, out_channels=in_channels, stride=2)
-            for _ in range(r - 1)
+            for _ in range(d - 1)
         ]
 
         last_layer = nn.Sequential(
@@ -121,29 +121,22 @@ class Down(nn.Sequential):
 class FusionStream(nn.Module):
     def __init__(self, r: int, channels: list[int]) -> None:
         super().__init__()
-        s = len(channels)
 
-        self.r = r
         self.channels = channels
         self.relu = nn.ReLU(inplace=True)
-        self.layers = nn.ModuleList(self._layer(i=i) for i in range(s))
+        self.layers = nn.ModuleList(self.fir(i=i, r=r) for i in range(len(channels)))
 
-    def _layer(self, i: int) -> nn.Module:
-        if i == self.r:
+    def fir(self, i: int, r: int) -> nn.Module:
+        c1, c2 = self.channels[i], self.channels[r]
+
+        if i == r:  # Id
             return nn.Identity()
 
-        if i < self.r:  # Down
-            return Down(
-                r=self.r - i,
-                in_channels=self.channels[i],
-                out_channels=self.channels[self.r],
-            )
+        if i < r:  # Down
+            return Down(d=r - i, in_channels=c1, out_channels=c2)
+
         # Up
-        return Up(
-            r=i - self.r,
-            in_channels=self.channels[i],
-            out_channels=self.channels[self.r],
-        )
+        return Up(d=i - r, in_channels=c1, out_channels=c2)
 
     def forward(self, inputs: list[torch.Tensor]) -> torch.Tensor:
         outputs = [layer(input) for input, layer in zip(inputs, self.layers)]
@@ -153,10 +146,9 @@ class FusionStream(nn.Module):
 class Fusion(nn.Module):
     def __init__(self, channels: list[int]) -> None:
         super().__init__()
-        s = len(channels)
 
         self.layers = nn.ModuleList(
-            FusionStream(r=r, channels=channels) for r in range(s)
+            FusionStream(r=r, channels=channels) for r in range(len(channels))
         )
 
     def forward(self, inputs: list[torch.Tensor]) -> list[torch.Tensor]:
@@ -164,17 +156,16 @@ class Fusion(nn.Module):
 
 
 class HRBranch(nn.Sequential):
-    def __init__(self, channels: int, reps: int):
-        super().__init__(*(ConvRes(channels=channels) for _ in range(reps)))
+    def __init__(self, channels: int, nb_convs: int):
+        super().__init__(*(ConvRes(channels=channels) for _ in range(nb_convs)))
 
 
 class HRBlock(nn.Module):
-    def __init__(self, channels: list[int], reps: list[int]):
+    def __init__(self, channels: list[int], nb_convs: list[int]):
         super().__init__()
-        s = len(channels)
 
         self.branches = nn.ModuleList(
-            HRBranch(channels=c, reps=r) for c, r in zip(channels, reps)
+            HRBranch(channels=c, nb_convs=n) for c, n in zip(channels, nb_convs)
         )
         self.fusion = Fusion(channels=channels)
 
@@ -186,28 +177,27 @@ class HRBlock(nn.Module):
 class Adaptor(nn.Module):
     def __init__(self, in_channels: list[int], out_channels: list[int]):
         super().__init__()
-        r = len(out_channels)
 
-        self.s = len(in_channels)
+        self.s_in, s_out = len(in_channels), len(out_channels)
         self.in_channels = in_channels
         self.out_channels = out_channels
 
-        self.layers = nn.ModuleList(self._layer(i) for i in range(r))
+        self.layers = nn.ModuleList(self.branch(i) for i in range(s_out))
 
-    def _layer(self, i: int) -> nn.Module:
-        if i == self.s:  # New stream
+    def branch(self, i: int) -> nn.Module:
+        if i == self.s_in:  # New branch
             return Conv3x3Block(
                 in_channels=self.in_channels[-1],
                 out_channels=self.out_channels[i],
                 stride=2,
             )
 
-        # Adapt #channels
-        if self.in_channels[i] != self.out_channels[i]:
+        if self.in_channels[i] != self.out_channels[i]:  # Adapt #channels
             return Conv3x3Block(
                 in_channels=self.in_channels[i], out_channels=self.out_channels[i]
             )
 
+        # No adaptation
         return nn.Identity()
 
     def forward(self, inputs: list[torch.Tensor]) -> list[torch.Tensor]:
@@ -225,7 +215,7 @@ class Stem(nn.Sequential):
 
 
 class FirstStage(nn.Sequential):
-    def __init__(self, in_channels: int, conv_reps: int, expansion: int = 4):
+    def __init__(self, in_channels: int, expansion: int = 4):
         out_channels = expansion * in_channels
         super().__init__(
             ExpandedBottleneck(channels=in_channels, expansion=expansion),
@@ -243,12 +233,15 @@ class Stage(nn.Sequential):
         self,
         in_channels: list[int],
         out_channels: list[int],
-        conv_reps: list[int],
-        reps: int,
+        nb_convs: list[int],
+        nb_blocks: int,
     ):
         super().__init__(
             Adaptor(in_channels=in_channels, out_channels=out_channels),
-            *(HRBlock(channels=out_channels, reps=conv_reps) for _ in range(reps)),
+            *(
+                HRBlock(channels=out_channels, nb_convs=nb_convs)
+                for _ in range(nb_blocks)
+            ),
         )
 
 
@@ -256,24 +249,24 @@ class HRNet32Backbone(nn.Sequential):
     def __init__(self, in_channels: int = 3):
         super().__init__(
             Stem(in_channels=in_channels, out_channels=64),
-            FirstStage(in_channels=64, conv_reps=4),
+            FirstStage(in_channels=64),
             Stage(  # 2
                 in_channels=[256],
                 out_channels=[32, 64],
-                conv_reps=[4, 4],
-                reps=1,
+                nb_convs=[4, 4],
+                nb_blocks=1,
             ),
             Stage(  # 3
                 in_channels=[32, 64],
                 out_channels=[32, 64, 128],
-                conv_reps=[4, 4, 4],
-                reps=4,
+                nb_convs=[4, 4, 4],
+                nb_blocks=4,
             ),
             Stage(  # 4
                 in_channels=[32, 64, 128],
                 out_channels=[32, 64, 128, 256],
-                conv_reps=[4, 4, 4, 4],
-                reps=3,
+                nb_convs=[4, 4, 4, 4],
+                nb_blocks=3,
             ),
         )
 
